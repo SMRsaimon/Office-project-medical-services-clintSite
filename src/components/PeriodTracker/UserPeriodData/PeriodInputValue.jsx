b8706f5e92d506011516f8235c7e8613d0d3fee6
SMRsaimon <smrsaimon960@gmail.com>
2021-06-15 02:48:59 -0700

    increment decrement done

--- src/components/PeriodTracker/UserPeriodData/PeriodInputValue.jsx
+++ src/components/PeriodTracker/UserPeriodData/PeriodInputValue.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const PeriodInputValue = () => {
+const PeriodInputValue = ({Value,hendelDecrement,hendelIncrement  }) => {
     return (
         <>
              <div className="spinner-Container  mx-auto">
-          <button className="btn decrement-btn">-</button>
+          <button  onClick={hendelDecrement} className="btn decrement-btn">-</button>
 
           <button className="periodStayCount" type="button">
-            <input type="button" value="1" name="periodStayCount" id="" />
+            <input type="button" value={Value}   name="periodStayCount" id="" />
           </button>
-          <button className="btn increment-btn">+</button>
+          <button onClick={hendelIncrement} className="btn increment-btn">+</button>
         </div>
         </>
     );
--- src/components/PeriodTracker/UserPeriodData/UserPeriodData.jsx
+++ src/components/PeriodTracker/UserPeriodData/UserPeriodData.jsx
@@ -7,9 +7,64 @@ import PeriodInputValue from "./PeriodInputValue";
 export const UserPeriodData = () => {
   const [date, setDate] = useState(new Date().toLocaleDateString());
 
+  const [DuraTion, setDuraTion] = useState(4);
+  const [cycleLong, setCycleLong] = useState(28);
+  const [periodData, setPeriodData] = useState({})
+
+
+//   Hendel Date change 
   const handelDateChange = (e) => {
     setDate(e.target.value);
+    
   };
+
+//   Hendel Duration Increment decrement 
+  const hendelDuraTionIncrement = () => {
+    setDuraTion((DuraTion) => {
+      const newCount = DuraTion >= 1 && DuraTion < 10 ? DuraTion + 1 : DuraTion;
+      return newCount;
+    });
+
+}
+const hendelDuraTionDecrement = () => {
+    setDuraTion((DuraTion) => {
+      const newCount = DuraTion >= 2 && DuraTion <= 10 ? DuraTion - 1 : DuraTion;
+      return newCount;
+    });
+
+}
+
+// Hendel Cycle increment decrement 
+    const hendelCycleIncrement = () => {
+      setCycleLong((cycleLong) => {
+        const newCount =
+          cycleLong >= 15 && cycleLong < 35 ? cycleLong + 1 : cycleLong;
+        return newCount;
+      });
+    };
+
+
+
+  const hendelCycleDeccrement = () => {
+    setCycleLong((cycleLong) => {
+        const newCount =
+          cycleLong >= 16 && cycleLong <= 35 ? cycleLong - 1 : cycleLong;
+        return newCount;
+      });
+
+
+  };
+
+//   hendel User PeriodData
+  const hendelUserPeriodData=()=>{
+
+      const data={date, DuraTion,cycleLong }
+      setPeriodData(data)
+
+
+  }
+
+  console.log(periodData)
 
   return (
     <div className="row my-5 py-5" id="UserData">
@@ -25,12 +80,22 @@ export const UserPeriodData = () => {
       </div>
       <div className="col-md-4">
         <DataTitle number={2} heading="How long did it last?" />
-        <PeriodInputValue/>
-       
+        <PeriodInputValue
+          Value={DuraTion}
+          hendelDecrement={hendelDuraTionDecrement}
+          hendelIncrement={hendelDuraTionIncrement}
+        />
       </div>
       <div className="col-md-4">
         <DataTitle number={3} heading="How long is your menstrual cycle?" />
-        <PeriodInputValue/>
+        <PeriodInputValue
+          Value={cycleLong}
+          hendelDecrement={hendelCycleDeccrement}
+          hendelIncrement={hendelCycleIncrement}
+        />
+      </div>
+      <div onClick={hendelUserPeriodData} className="col-12 d-flex justify-content-center mt-5 ">
+          <button className="btn btn-primary"  type="button"> Get Result </button>
       </div>
     </div>
   );
