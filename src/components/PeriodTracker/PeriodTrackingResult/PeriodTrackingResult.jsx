0d0d2c43a8e6ac010fb64a61b5b87c4d4fd61f2f
SMRsaimon <smrsaimon960@gmail.com>
2021-06-17 01:05:59 -0700

    date selected done

--- src/components/PeriodTracker/PeriodTrackingResult/PeriodTrackingResult.jsx
+++ src/components/PeriodTracker/PeriodTrackingResult/PeriodTrackingResult.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-extend-native */
 import React, { useState } from "react";
 import { useEffect } from "react";
 import PeriodBanner from "../PeriodBanner/PeriodBanner";
@@ -5,41 +6,92 @@ import "./PeriodTrackingResult.css";
 import DayPicker from "react-day-picker";
 import "react-day-picker/lib/style.css";
 import { periodCalculation } from "./PeriodCalculation";
-
+import { FaLessThanEqual } from "react-icons/fa";
 
 const PeriodTrackingResult = () => {
   const [periodData, setPeriodData] = useState({});
   useEffect(() => {
     const data = localStorage.getItem("data");
     setPeriodData(JSON.parse(data));
-   
   }, []);
-// date, DuraTion,cycleLong
+  // date, DuraTion,cycleLong
 
- const periodCalculateData=periodCalculation(periodData.DuraTion, periodData.cycleLong, periodData.date )
- console.log(periodCalculateData)
+  const periodCalculateData = periodCalculation(
+    periodData.DuraTion,
+    periodData.cycleLong,
+    periodData.date
+  );
 
+  
+  const [,,,target]=periodCalculateData
+
+ const {begin, end }=target
+
+//  get array of dates between 2 dates
+Date.prototype.addDays = function(days) {
+  let  dat = new Date(this.valueOf())
+  dat.setDate(dat.getDate() + days);
+  return dat;
+}
+
+function getDates(startDate, stopDate) {
+ const  dateArray = [];
+ let  currentDate = startDate;
+ while (currentDate <= stopDate) {
+   dateArray.push(currentDate)
+   currentDate = currentDate.addDays(1);
+ }
+ return dateArray;
+}
+
+
+const  dateArray = getDates(new Date(begin), (new Date(end)));
+
+
+
+
+
+ 
 
   const modifiers = {
-    thursdays: { daysOfWeek: [1,4] },
-    birthday: new Date(),
+    thursdays: { daysOfWeek: [1, 4] },
+    // birthday: new Date(),
+    possible_period_dates_Begin:new Date(dateArray[0]),
+    possible_period_dates_end: new Date(dateArray[1]),
+    possible_period_dates3:new Date(dateArray[2]),
+      
+
+    
   };
   const modifiersStyles = {
-    birthday: {
+    // birthday: {
+    //   color: "white",
+    //   backgroundColor: "#ffc107",
+    // },
+    possible_period_dates_Begin: {
       color: "white",
       backgroundColor: "#ffc107",
     },
-  
-    thursdays: {
-      color: "#ffc107",
-      backgroundColor: "#3f51b5ab",
+    possible_period_dates_end:{
+      color: "white",
+      backgroundColor: "#ffc107",
+
     },
-   
+    possible_period_dates3:{
+      color: "white",
+      backgroundColor: "#ffc107",
+
+    }
+
+    // thursdays: {
+    //   color: "#ffc107",
+    //   backgroundColor: "#3f51b5ab",
+    // },
   };
 
   return (
     <>
-      <div className="container mb-5" id="DayPicker-custom-style"  >
+      <div className="container mb-5" id="DayPicker-custom-style">
         <PeriodBanner />
 
         <div className="row d-flex mt-5 pt-5">
